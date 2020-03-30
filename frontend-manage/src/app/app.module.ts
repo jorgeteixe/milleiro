48545cdd4513c0d4b4b4468c133baeaf4aa7be76
jorgeteixe <jorgeteixe11@gmail.com>
2020-03-30 16:34:44 +0200

    added fontawesome and favicon

--- frontend-manage/src/app/app.module.ts
+++ frontend-manage/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
